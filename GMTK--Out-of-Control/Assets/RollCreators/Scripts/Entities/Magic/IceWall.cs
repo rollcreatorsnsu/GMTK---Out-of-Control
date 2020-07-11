1251b59f5afecfe0c026ee25c5c73f713f958b90
zhuravlev12 <slawajur@mail.ru>
2020-07-11 17:06:10 +0700

    added lightning

--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Magic/IceWall.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Magic/IceWall.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IceWall : MonoBehaviour
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+public class IceWall : MonoBehaviour, IMagic
 {
+    [SerializeField] private float secondsToLife = 7;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Destroy(gameObject, secondsToLife);
     }
 
     // Update is called once per frame
--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Magic/LightningStrike.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Magic/LightningStrike.cs
@@ -1,18 +1,70 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Comparers;
 
-public class LightningStrike : MonoBehaviour
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+public class LightningStrike : MonoBehaviour, IMagic
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private float damage;
+    private HashSet<GameObject> enemies = new HashSet<GameObject>();
+    private GameObject player;
+    private bool updated = false;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!updated)
+        {
+            updated = true;
+            return;
+        }
+        float minDistance = Single.PositiveInfinity;
+        GameObject nearestEnemy = null;
+        foreach (GameObject enemyOnj in enemies)
+        {
+            float distance = Vector3.Distance(transform.position, enemyOnj.transform.position);
+            if (distance < minDistance)
+            {
+                nearestEnemy = enemyOnj;
+                minDistance = distance;
+            }
+        }
+
+        if (nearestEnemy == null)
+        {
+            HitPlayer(player.GetComponent<Player>());
+        }
+        else
+        {
+            HitEnemy(nearestEnemy.GetComponent<Enemy>());
+        }
+    }
+
+    private void HitPlayer(Player player)
+    {
+        player.Hit(damage);
+        Destroy(gameObject);
+    }
+    
+    private void HitEnemy(Enemy enemy)
+    {
+        transform.position = enemy.transform.position;
+        enemy.Hit(damage);
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            enemies.Add(other.gameObject);
+        }
+        else if (other.gameObject.CompareTag("Player"))
+        {
+            player = other.gameObject;
+        }
     }
 }
