64a80d39a1c894190c66422425cf4020ed8e593d
zhuravlev12 <slawajur@mail.ru>
2020-07-11 15:35:55 +0700

    added enemies

--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Enemies/Bat.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Enemies/Bat.cs
@@ -15,4 +15,9 @@ public class Bat : Enemy
     {
         
     }
+
+    protected override void Move()
+    {
+        transform.position += (player.transform.position - transform.position).normalized * Time.deltaTime;
+    }
 }
--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Enemies/Skeleton.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Enemies/Skeleton.cs
@@ -15,4 +15,9 @@ public class Skeleton : Enemy
     {
         
     }
+    
+    protected override void Move()
+    {
+        transform.position += (player.transform.position - transform.position).normalized * Time.deltaTime;
+    }
 }
