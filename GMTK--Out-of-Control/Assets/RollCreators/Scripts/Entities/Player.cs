dac8c0d233251c996d07611fd0e5384cf5fbe052
zhuravlev12 <slawajur@mail.ru>
2020-07-12 16:38:38 +0700

    added player animations

--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Player.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Player.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D), typeof(Animator))]
 public class Player : MonoBehaviour
 {
     [Serializable]
@@ -18,6 +18,16 @@ public class Player : MonoBehaviour
     public delegate void HitHandler(float health);
     public static event HitHandler PlayerIsHit;
     
+    private static Dictionary<Magic, string> magicAnimationNames = new Dictionary<Magic, string>();
+
+    static Player()
+    {
+        magicAnimationNames.Add(Magic.FIREBALL, "Fireball");
+        magicAnimationNames.Add(Magic.ICE_WALL, "Ice Wall");
+        magicAnimationNames.Add(Magic.LIGHTNING_STRIKE, "Lightning Strike");
+        magicAnimationNames.Add(Magic.NATURE_RISES, "Nature Rises");
+    }
+    
     [SerializeField] private float health;
     [SerializeField] private float hitTime = 0.5f;
     [SerializeField] private float attackTime = 0.5f;
@@ -26,10 +36,12 @@ public class Player : MonoBehaviour
     private float lastAttack = 0;
     private Magic magic;
     private Dictionary<Magic, GameObject> magics = new Dictionary<Magic, GameObject>();
+    private Animator animator;
     
     // Start is called before the first frame update
     void Start()
     {
+        animator = GetComponent<Animator>();
         foreach (MagicMapItem magicItem in magicsList)
         {
             magics.Add(magicItem.magicEnum, magicItem.magicObject);
@@ -43,16 +55,18 @@ public class Player : MonoBehaviour
         if (Input.GetKeyUp(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left * Time.deltaTime;
+            Walk();
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.position += Vector3.right * Time.deltaTime;
+            Walk();
         }
         
         if (Input.GetKeyUp(KeyCode.Z) || Input.GetKeyUp(KeyCode.UpArrow))
         {
-            transform.position += Vector3.up;
+            animator.Play("Jump");
         }
 
         if (Input.GetKeyUp(KeyCode.X))
@@ -61,15 +75,22 @@ public class Player : MonoBehaviour
         }
     }
 
+    private void Walk()
+    {
+        animator.Play("Walking");
+    }
+
     private void Attack()
     {
         if (Time.time - lastAttack < attackTime) return;
+        animator.Play(magicAnimationNames[magic]);
         lastAttack = Time.time;
         Instantiate(magics[magic]);
     }
 
     private void Die()
     {
+        animator.Play("Die");
         PlayerIsDead.Invoke();
     }
 
@@ -81,6 +102,7 @@ public class Player : MonoBehaviour
     public void Hit(float damage)
     {
         if (Time.time - lastHit < hitTime) return;
+        animator.Play("Hit");
         lastHit = Time.time;
         health -= damage;
         PlayerIsHit.Invoke(health);
