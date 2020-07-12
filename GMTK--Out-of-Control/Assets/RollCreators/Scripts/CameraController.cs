f6a9f4ce4c567c68bf0aaec1aa1455d73fd44a3d
zhuravlev12 <slawajur@mail.ru>
2020-07-13 01:05:57 +0700

    added camera

--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/CameraController.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/CameraController.cs
@@ -6,6 +6,14 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Player player;
     private void Update()
     {
-        
+        float playerX = Camera.main.WorldToViewportPoint(player.transform.position).x;
+        if (playerX < 0.1)
+        {
+            Camera.main.transform.position += Vector3.left * Time.deltaTime;
+        }
+        else if (playerX > 0.9)
+        {
+            Camera.main.transform.position += Vector3.right * Time.deltaTime;
+        }
     }
 }
