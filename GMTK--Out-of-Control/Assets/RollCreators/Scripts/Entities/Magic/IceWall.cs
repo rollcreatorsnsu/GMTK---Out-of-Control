5736111d730224ff43abe933d0cb02a393f8ae82
zhuravlev12 <slawajur@mail.ru>
2020-07-12 16:45:52 +0700

    added magic animations

--- GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Magic/IceWall.cs
+++ GMTK--Out-of-Control/Assets/RollCreators/Scripts/Entities/Magic/IceWall.cs
@@ -1,13 +1,23 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
-[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D))]
+[RequireComponent(typeof(Collider2D), typeof(Rigidbody2D), typeof(Animator))]
 public class IceWall : MonoBehaviour, IMagic
 {
+    [SerializeField] private Animator animator;
     [SerializeField] private float secondsToLife = 7;
     
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, secondsToLife);
+        animator = GetComponent<Animator>();
+        StartCoroutine(Fall());
+    }
+
+    private IEnumerator Fall()
+    {
+        yield return new WaitForSeconds(secondsToLife);
+        animator.Play("Ice Wall_destroy");
     }
 }
